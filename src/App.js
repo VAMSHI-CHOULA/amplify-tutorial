a3ea4648f2610b54d8b41777b45f598b3ff4baba
VAMSHI-CHOULA <516vamshichoula516@gmail.com>
2025-09-07 12:58:18 +0530

    changes moving to github repository2

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          Hello World
+          Hello amplify 
         </a>
       </header>
     </div>
