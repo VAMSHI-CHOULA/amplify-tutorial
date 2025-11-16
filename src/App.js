5e4b8223160abf821761a0906f7c953a8c69287e
VAMSHI-CHOULA <516vamshichoula516@gmail.com>
2025-11-17 00:50:48 +0530

    Moving changes from local to remote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import RazorpayButton from './RazorpayButton';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Hello amplify . How are you.
         </a>
       </header>
+      <div>
+        <RazorpayButton />
+      </div>
     </div>
   );
 }
